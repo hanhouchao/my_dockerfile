0f74d9545c729a01d4cb17639b52022c8f1b12a0
shibin <1220642745@qq.com>
2016-07-14 17:05:35 +0800

    update Dockerfile

--- svn/svn.sh
+++ svn/svn.sh
@@ -10,10 +10,11 @@ fi
 
 mkdir -p $SVN_REPO_PATH
 svnadmin create $SVN_REPO_PATH/$SVN_REPO_NAME
+chmod -R  a+w $SVN_REPO_PATH/$SVN_REPO_NAME
 
 cat >> /etc/apache2/apache2.conf  <<EOF
   LoadModule dav_module modules/mod_dav.so
-  LoadModule dav_svn_module modules/mod_dav_svn.so
+  LoadModule dav_svn_module modules/mod_dav_svn.so 
   LoadModule authz_svn_module modules/mod_authz_svn.so
   <Location /$SVN_REPO_NAME>
       DAV svn
